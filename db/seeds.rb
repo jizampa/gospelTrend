77dcb2fbf26e104a39479697a1f75df6882dbe5c
jizampa <jizampa@gmail.com>
2018-10-28 01:08:36 +0000

    tag seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Province.destroy_all
+Tag.destroy_all
 
 tagArray = ["paid", "processing", "fulfilled", "delivered", "canceled"]
 
