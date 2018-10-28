2482b5fc21e7b960307e7c7065423a5f1bd67cc0
jizampa <jizampa@gmail.com>
2018-10-28 00:47:30 +0000

    seed for provinces

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Province.destroy_all
+
+provincesArray = [
+                    ["Alberta","AB",0,5,0,"Canada"],
+                    ["British Columbia","BC",7,5,0,"Canada"],
+                    ["Manitoba","MB",8,5,0,"Canada"],
+                    ["New-Brunswick","NB",0,0,15,"Canada"],
+                    ["Newfoundland and Labrador","NL",0,0,15,"Canada"],
+                    ["Northwest Territories","NT",0,5,0,"Canada"],
+                    ["Nova Scotia","NS",0,0,15,"Canada"],
+                    ["Nunavut","NU",0,5,0,"Canada"],
+                    ["Ontario","ON",0,0,13,"Canada"],
+                    ["Prince Edward Island","PE",0,0,15,"Canada"],
+                    ["Québec","QC",9.975,5,0,"Canada"],
+                    ["Saskatchewan","SK",6,5,0,"Canada"],
+                    ["Yukon","YT",0,5,0,"Canada"]
+                    
+]
+
+provincesArray.each do |prov|
+    newProv = Province.new(:name => prov[0],
+                            :acronyms => prov[1],
+                            :PST => prov[2],
+                            :GST => prov[3],
+                            :HST => prov[4],
+                            :country => prov[5])
+    newProv.save!
+    end
