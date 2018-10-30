87bd45e27f72d7d540850669a8ddba2770dc5d6c
jizampa <jizampa@gmail.com>
2018-10-30 13:04:01 +0000

    fixed new tag from admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,4 @@
-ActiveAdmin.register Tag do
+ActiveAdmin.register User do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -11,5 +11,5 @@ ActiveAdmin.register Tag do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
-permit_params :name
+
 end
